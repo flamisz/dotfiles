81939e53eb3a7fa89e5d883369e088c1513d0fd8
Zoltan Flamis <zoltan@hey.com>
2021-06-07 07:41:42 +1200

    keys

--- path.zsh
+++ path.zsh
@@ -7,6 +7,9 @@ export PATH="$HOME/.node/bin:$PATH"
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 
+# homebrew
+export PATH="/opt/homebrew/sbin:$PATH"
+
 # Make sure coreutils are loaded before system commands
 # I've disabled this for now because I only use "ls" which is
 # referenced in my aliases.zsh file directly.
